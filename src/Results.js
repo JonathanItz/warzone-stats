e2b0188169dac9e95a811246a8fe4373e8c4bedf
Jonathan Itzen <jonathanitzen95@gmail.com>
2020-10-08 20:53:25 -0500

    edits

--- src/Results.js
+++ src/Results.js
@@ -19,60 +19,60 @@ export default function Results( { stats, username, platform } ) {
             <div className="row stats-row">
                 <div className="stat-container columns four">
                     <div className="heading columns six">Wins</div>
-                    <div className="stat columns six">{ stats.wins }</div>
+                    <div className="stat columns six">{ stats.wins ? stats.wins : '--' }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Top Five</div>
-                    <div className="stat columns six">{ stats.topFive }</div>
+                    <div className="stat columns six">{ stats.topFive ? stats.topFive : '--' }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Top Ten</div>
-                    <div className="stat columns six">{ stats.topTen }</div>
+                    <div className="stat columns six">{ stats.topTen ? stats.topTen : '--' }</div>
                 </div>
             </div>
 
             <div className="row stats-row">
                 <div className="stat-container columns four">
                     <div className="heading columns six">KD</div>
-                    <div className="stat columns six">{ roundNumber( stats.kdRatio ) }</div>
+                    <div className="stat columns six">{ roundNumber( stats.kdRatio ? stats.kdRatio : 0 ) }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Kills</div>
-                    <div className="stat columns six">{ stats.kills }</div>
+                    <div className="stat columns six">{ stats.kills ? stats.kills : '--' }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Deaths</div>
-                    <div className="stat columns six">{ stats.deaths }</div>
+                    <div className="stat columns six">{ stats.deaths ? stats.deaths : '--' }</div>
                 </div>
             </div>
 
             <div className="row stats-row">
                 <div className="stat-container columns four">
                     <div className="heading columns six">Score</div>
-                    <div className="stat columns six">{ stats.score }</div>
+                    <div className="stat columns six">{ stats.score ? stats.score : '--' }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Revives</div>
-                    <div className="stat columns six">{ stats.revives }</div>
+                    <div className="stat columns six">{ stats.revives ? stats.revives : '--' }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Downs</div>
-                    <div className="stat columns six">{ stats.downs }</div>
+                    <div className="stat columns six">{ stats.downs ? stats.downs : '--' }</div>
                 </div>
             </div>
 
             <div className="row stats-row">
                 <div className="stat-container columns four">
                     <div className="heading columns six">Score Per Minute</div>
-                    <div className="stat columns six">{ roundNumber( stats.scorePerMinute ) }</div>
+                    <div className="stat columns six">{ roundNumber( stats.scorePerMinute ? stats.scorePerMinute : 0 ) }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Total Games</div>
-                    <div className="stat columns six">{ stats.gamesPlayed }</div>
+                    <div className="stat columns six">{ stats.gamesPlayed ? stats.gamesPlayed : '--' }</div>
                 </div>
                 <div className="stat-container columns four">
                     <div className="heading columns six">Time Played</div>
-                    <div className="stat columns six">{ stats.timePlayed }</div>
+                    <div className="stat columns six">{ stats.timePlayed ? stats.timePlayed : '--' }</div>
                 </div>
             </div>
 
