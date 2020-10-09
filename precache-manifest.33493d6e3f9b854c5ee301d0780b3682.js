3c8db82f55a2b181ae9c67ef97115e5b052ad096
Jonathan Itzen <jonathanitzen95@gmail.com>
2020-10-08 20:08:47 -0500

    Updates

---
+++ precache-manifest.33493d6e3f9b854c5ee301d0780b3682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd68758f77232bce79cf732504c09a2",
+    "url": "/warzone-stats/index.html"
+  },
+  {
+    "revision": "3db68984c692457d68a1",
+    "url": "/warzone-stats/static/css/main.d5dc975f.chunk.css"
+  },
+  {
+    "revision": "caa8bca52f00eef37fa5",
+    "url": "/warzone-stats/static/js/2.48420d6f.chunk.js"
+  },
+  {
+    "revision": "df94fdf0a632800991574f45ecb81836",
+    "url": "/warzone-stats/static/js/2.48420d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db68984c692457d68a1",
+    "url": "/warzone-stats/static/js/main.287bf0c8.chunk.js"
+  },
+  {
+    "revision": "7e2950d5048a672c7b68",
+    "url": "/warzone-stats/static/js/runtime-main.023da222.js"
+  }
+]);
